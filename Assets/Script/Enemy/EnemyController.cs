7835b68867bb3a6c384f1acab3a00c88984fa89c
Tran Hiep <myname8102@gmail.com>
2024-04-02 21:46:19 +0700

    Create new enemy

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -7,9 +7,12 @@ public class EnemyController : Object
     Rigidbody2D obj;
 
     float timer;
-    public float changeTime = 2.0f;
+    public float changeTime;
+    public bool isRocket;
     int direction = 1;
     float moveTimer;
+
+    Animator ani;
 
     //Vector2 currentPosition;
     public GameObject bullet;
@@ -22,11 +25,10 @@ public class EnemyController : Object
     void Start()
     {
         obj = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
         //audioSource = GetComponent<AudioSource>();
         //currentPosition = obj.position;
         timer = Random.Range(1f, 3f);
-        speed = 2f;
-        moveTimer = 1f;
     }
 
     // Update is called once per frame
@@ -35,7 +37,16 @@ public class EnemyController : Object
         timer -= Time.deltaTime;
         if (timer < 0)
         {
-            Launch();
+            if (isRocket)
+            {
+                StartCoroutine(LaunchRocket());
+                StartCoroutine(ShootingAnimation(1.2f));
+            }
+            else
+            {
+                Launch();
+            }
+            timer = Random.Range(2f, 5f);
         }
 
         moveTimer -= Time.deltaTime;
@@ -57,7 +68,29 @@ public class EnemyController : Object
     {
         //audioSource.PlayOneShot(shootSound);
         Instantiate(bullet, obj.position - Vector2.up * 0.5f, Quaternion.identity);
-        timer = Random.Range(2f, 5f);
+    }
+
+    IEnumerator LaunchRocket()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Instantiate(bullet, transform.position + new Vector3(-0.25f, 0, 0), Quaternion.Euler(0, 0, -5));
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(bullet, transform.position + new Vector3(0.25f, 0, 0), Quaternion.Euler(0, 0, 5));
+        yield return new WaitForSeconds(0.15f);
+        Instantiate(bullet, transform.position + new Vector3(-0.45f, 0, 0), Quaternion.Euler(0, 0, -10));
+        yield return new WaitForSeconds(0.15f);
+        Instantiate(bullet, transform.position + new Vector3(0.45f, 0, 0), Quaternion.Euler(0, 0, 10));
+        yield return new WaitForSeconds(0.15f);
+        Instantiate(bullet, transform.position + new Vector3(-0.65f, 0, 0), Quaternion.Euler(0, 0, -15));
+        yield return new WaitForSeconds(0.15f);
+        Instantiate(bullet, transform.position + new Vector3(0.65f, 0, 0), Quaternion.Euler(0, 0, 15));
+    }
+
+    IEnumerator ShootingAnimation(float animationTimer)
+    {
+        ani.SetBool("isShooting", true);
+        yield return new WaitForSeconds(animationTimer);
+        ani.SetBool("isShooting", false);
     }
 
     public void Hitted()
